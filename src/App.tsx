2509114e5556a9aba3a83f98ca98b7efba48e2ba
Maycon Sousa <mayconfsousa@Maycons-MacBook-Pro.local>
2018-05-16 12:28:46 -0300

    Adding react-native firebase configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
 import * as React from 'react';
+import firebase from 'react-native-firebase';
 
 import HomeScreen from '@screens/HomeScreen';
 
 export default class App extends React.Component<{}, {}> {
+  async componentDidMount() {
+    const fcmToken = await firebase.messaging().getToken();
+    if (fcmToken) {
+      console.log('SUCCESS', fcmToken);
+    } else {
+      console.log('ERROR', fcmToken);
+    }
+
+    const enabled = await firebase.messaging().hasPermission();
+    if (enabled) {
+      console.log('user has permissions');
+    } else {
+      console.log('user doesnt have permission');
+    }
+
+    try {
+      await firebase.messaging().requestPermission();
+      console.log('User has authorised');
+    } catch (error) {
+      console.log('User has rejected permissions');
+    }
+  }
+
   render() {
     return <HomeScreen title="Hi" />;
   }
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,13 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 
+// import type { RemoteMessage } from 'react-native-firebase';
+
+const bgMessaging = async message => {
+  console.log('bgMessaging', message);
+  return Promise.resolve();
+};
+
 AppRegistry.registerComponent('IotAlpr', () => App);
+
+AppRegistry.registerHeadlessTask('RNFirebaseBackgroundMessage', () => bgMessaging);
